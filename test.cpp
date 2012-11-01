91e7988edd096150aed15ec53cd9c36067608ed0
Mike Plavsky <Mike.Plavsky@quest.com>
2012-11-01 22:10:14 +0000

    start

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+string message(string name) {
+
+  ostringstream res;
+  res << "Hello, " << name << "!" << endl;
+  
+  return res.str();  
+
+}
+
+int main() {
+
+  cout << "What's your name?" << endl;
+
+  string n;
+  cin >> n;
+
+  cout << message( n );
+
+  vector<int> a3{1,2,3};
+
+  for( auto i: a3){
+      cout << i << endl;
+
+}
